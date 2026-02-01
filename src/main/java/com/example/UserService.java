fb3ad5b4b9d955b041f76b2f7c4a9c429542f505
paranietharan <142080100+paranietharan@users.noreply.github.com>
2026-02-01 12:35:29 +0530

    Introduce quality issues

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -12,16 +12,22 @@ public class UserService {
     // VULNERABILITY: SQL Injection
     public void findUser(String username) throws Exception {
 
-        Connection conn =
-            DriverManager.getConnection("jdbc:mysql://localhost/db",
-                    "root", password);
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db",
+                "root", password);
 
         Statement st = conn.createStatement();
 
-        String query =
-            "SELECT * FROM users WHERE name = '" + username + "'";
+        String query = "SELECT * FROM users WHERE name = '" + username + "'";
 
         st.executeQuery(query);
+    }
+
+    public void deleteUser(String username) throws Exception {
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db",
+                "root", password);
+        Statement st = conn.createStatement();
+        String query = "DELETE FROM users WHERE name = '" + username + "'";
+        st.execute(query);
     }
 
     // SMELL: Unused method
